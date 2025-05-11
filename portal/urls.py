f81a7058fb6abbed044417e2878f06daec4ed9e5
Aishwarya C G <aishuaishwaryacg@gmail.com>
2025-05-11 13:38:12 +0530

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('signup/', views.signup, name='signup'),
+   
     path('complaint/new/', views.create_complaint, name='create_complaint'),
     path('complaint/<int:pk>/', views.complaint_detail, name='complaint_detail'),
     path('profile/', views.profile, name='profile'),
