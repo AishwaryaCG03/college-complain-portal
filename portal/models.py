ed986e3c7ede38331a54d63549ad4f4021a71fc6
Aishwarya C G <aishuaishwaryacg@gmail.com>
2025-05-11 13:37:40 +0530

    Update models.py

--- portal/models.py
+++ portal/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     USER_ROLES = (
@@ -27,11 +29,7 @@ class Category(models.Model):
         return self.name
 
 class Complaint(models.Model):
-    STATUS_CHOICES = (
-        ('Pending', 'Pending'),
-        ('Escalated', 'Escalated'),
-        ('Resolved', 'Resolved'),
-    )
+    
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
     anonymous = models.BooleanField(default=False)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
@@ -39,10 +37,23 @@ class Complaint(models.Model):
     evidence = models.FileField(upload_to='evidence/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     resolved_at = models.DateTimeField(null=True, blank=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    status = models.CharField(max_length=20, choices=(
+    ('Pending', 'Pending'),
+    ('Escalated', 'Escalated'),
+    ('Resolved', 'Resolved'),
+    ), default='Pending')
     rating = models.PositiveIntegerField(null=True, blank=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
     escalation_level = models.PositiveIntegerField(default=0)  # 0: initial, 1: class teacher, 2: HOD, 3: Principal
-    email_sent = models.BooleanField(default=False)  # New field to track email notification
+    email_sent = models.BooleanField(default=False) 
+    sentiment = models.FloatField(null=True, blank=True)
+    
+    
+    
+    assigned_to = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='assigned_complaints')
+
+    
+    
+     # New field to track email notification
     def is_overdue(self):
         if self.status != 'Resolved' and (timezone.now() - self.created_at).days > 14:
             return True
@@ -56,3 +67,11 @@ class Complaint(models.Model):
 
     def __str__(self):
         return f"Complaint {self.id} - {self.category.name} - {self.status}"
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
