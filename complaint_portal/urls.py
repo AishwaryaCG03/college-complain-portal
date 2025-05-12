31e8206a8588841c9197fb73eda54b92669be40f
Aishwarya C G <aishuaishwaryacg@gmail.com>
2025-05-12 17:51:15 +0530

    Update urls.py

--- complaint_portal/urls.py
+++ complaint_portal/urls.py
@@ -2,12 +2,26 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from portal import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portal.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  # for login/logout
+    path('accounts/', include('django.contrib.auth.urls')), 
+     path('complaint/<int:pk>/', views.complaint_detail, name='complaint_detail'),
+      path('profile/', views.profile, name='profile'),
+      
+    path('admin/', admin.site.urls),
+    path('portal/', include('portal.urls')),  # Include your app's URLs
+    # Password reset URLs
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
+
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
